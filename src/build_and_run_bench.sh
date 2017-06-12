76887739558bd833e5322897416121b8fa6a7b40
Mattia92 <mattia.crippa.brqw@alice.it>
2017-06-12 19:01:52 +0200

    Added mac/Linux .sh files to build and run both benchmark and test suite

---
+++ src/build_and_run_bench.sh
@@ -0,0 +1,9 @@
+function pause(){
+read -n1 -r -p "Press any key to continue..." key
+}
+
+clang -Wall -Wextra -O3 -c scapegoat.c -o scapegoat.lib
+clang++ -Wall -Wextra -O3 bench.cpp scapegoat.lib -o bench
+pause
+bench
+pause
