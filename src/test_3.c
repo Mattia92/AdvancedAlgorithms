4d7316b07477838beea7315bf9f8d24572dfed03
Mattia92 <mattia.crippa.brqw@alice.it>
2017-06-12 20:17:38 +0200

    fix test_3 with new heuristic

--- src/test_3.c
+++ src/test_3.c
@@ -41,14 +41,17 @@ int main(void) {
 
 	// check rebalance on insert (scapegoat is right child)
 	check_insert(tree, 37, TRUE);
-	check_node(tree->root->left, 20, FALSE, 0, TRUE, 35);
-	check_node(tree->root->left->right, 35, TRUE, 25, TRUE, 40);
-	check_node(tree->root->left->right->left, 25, TRUE, 22, TRUE, 30);
-	check_node(tree->root->left->right->left->left, 22, FALSE, 0, FALSE, 0);
-	check_node(tree->root->left->right->left->right, 30, FALSE, 0, FALSE, 0);
-	check_node(tree->root->left->right->right, 40, TRUE, 37, TRUE, 50);
-	check_node(tree->root->left->right->right->left, 37, FALSE, 0, FALSE, 0);
-	check_node(tree->root->left->right->right->right, 50, FALSE, 0, FALSE, 0);
+	check_node(tree->root, 37, TRUE, 25, TRUE, 100);
+	check_node(tree->root->left, 25, TRUE, 22, TRUE, 35);
+	check_node(tree->root->left->left, 22, TRUE, 20, FALSE, 0);
+	check_node(tree->root->left->left->left, 20, FALSE, 0, FALSE, 0);
+	check_node(tree->root->left->right, 35, TRUE, 30, FALSE, 0);
+	check_node(tree->root->left->right->left, 30, FALSE, 0, FALSE, 0);
+	check_node(tree->root->right, 100, TRUE, 50, TRUE, 180);
+	check_node(tree->root->right->left, 50, TRUE, 40, FALSE, 0);
+	check_node(tree->root->right->left->left, 40, FALSE, 0, FALSE, 0);
+	check_node(tree->root->right->right, 180, TRUE, 160, FALSE, 0);
+	check_node(tree->root->right->right->left, 160, FALSE, 0, FALSE, 0);
 
 	sg_delete_tree(tree);
 
