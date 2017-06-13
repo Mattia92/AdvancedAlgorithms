d271e3dde84520d8fdfc5b0dc0a7e248e4aa172f
AlessandroVetere <alessandro.vetere@mail.polimi.it>
2017-06-13 14:06:30 +0200

    .sh file for bench mem.

---
+++ src/build_and_run_bench_mem.sh
@@ -0,0 +1,12 @@
+function pause() {
+	read -n1 -r -p "Press any key to continue..." key
+}
+
+clang -Wall -Wextra -O3 -o bench_sg_mem bench_sg_mem.c scapegoat.c
+pause
+./bench_sg_mem
+pause
+clang++ -Wall -Wextra -O3 bench_rb_mem.cpp scapegoat.lib -o bench_rb_mem
+pause
+./bench_rb_mem
+pause
