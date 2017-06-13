f9984170efe70ab7b8898b28d5ba2efe0691f0e7
AlessandroVetere <alessandro.vetere@mail.polimi.it>
2017-06-13 14:37:51 +0200

    Multiplatform solution to replace scanf_s.

--- src/bench_rb_mem.cpp
+++ src/bench_rb_mem.cpp
@@ -1,13 +1,14 @@
-#include <stdio.h> /* printf, scanf_s */
+#include <stdio.h> /* printf, fgets */
 #include <set> /* std::set<int> */
 
 #define M 10
 #define N M * 1000 * 1000
+#define BUF_SIZE 2
 
 int main(void) {
 	std::set<int> rb_tree;
 	unsigned int i;
-	char d;
+	char d[BUF_SIZE];
 
 	printf("=== BENCHMARK RB MEM - START ===\n");
 
@@ -19,9 +20,10 @@ int main(void) {
 
 	printf("BENCHMARK RB MEM - Insertions done, measure memory usage then press any key to exit\n");
 
-	scanf_s("%c", &d);
+	// Read at most BUF_SIZE - 1 character, stopping after EOF or '\n' from stdin and store '\0' as BUF_SIZE char
+	fgets(d, BUF_SIZE, stdin);
 
-	rb_tree.clear();
+	rb_tree.clear();	
 
 	printf("=== BENCHMARK RB MEM - END ===\n");
 
--- src/bench_sg_mem.c
+++ src/bench_sg_mem.c
@@ -1,14 +1,15 @@
-#include <stdio.h> /* printf, scanf_s */
+#include <stdio.h> /* printf, fgets */
 
 #include "scapegoat.h"
 
 #define M 10
 #define N M * 1000 * 1000
+#define BUF_SIZE 2
 
 int main(void) {
 	struct sg_tree* sg_tree;
 	unsigned int i;
-	char d;
+	char d[BUF_SIZE];
 
 	printf("=== BENCHMARK SG MEM - START ===\n");
 
@@ -22,7 +23,8 @@ int main(void) {
 
 	printf("BENCHMARK SG MEM - Insertions done, measure memory usage then press any key to exit\n");
 
-	scanf_s("%c", &d);
+	// Read at most BUF_SIZE - 1 character, stopping after EOF or '\n' from stdin and store '\0' as BUF_SIZE char
+	fgets(d, BUF_SIZE, stdin);
 
 	printf("=== BENCHMARK SG MEM - END ===\n");
 
